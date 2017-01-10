9d2ddd10767eab987f77db0a92f321aa8a51983c
Thomas Morgana <tmorgana64@gmail.com>
2017-01-10 15:37:47 +0100

    Add moteur library

---
+++ MoteurLib/Moteur.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoteurLib
+{
+    public class Moteur
+    {
+        List<int> randList = new List<int>();
+
+        private void getRandomNumbers(List<int> randList)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                if (i >= 0 && i < 3)
+                    randList.Add(rand.Next(1, 13));
+                else if (i >= 3 && i < 5)
+                    randList.Add(rand.Next(1, 21));
+            }
+            this.getTargetNumber(randList);
+        }
+
+        private int getTargetNumber(List<int> random)
+        {
+            List<int> newList = new List<int>(random);
+            Random rand = new Random();
+            int randCalcul = rand.Next(2, 6);
+            List<string> history = new List<string>();
+
+            for (int i = 0; i < randCalcul; i++)
+            {
+                int rand1 = rand.Next(0, newList.Count);
+                int rand2 = rand.Next(0, newList.Count);
+                int randOperator = rand.Next(1, 5);
+
+                while (rand1 == rand2)
+                {
+                    rand2 = rand.Next(0, newList.Count);
+                }
+
+                switch (randOperator)
+                {
+                    case 1:
+                        history.Add(newList[rand1] + " + " + newList[rand2] + " = " + (newList[rand1] + newList[rand2]));
+                        newList.Add(newList[rand1] + newList[rand2]);
+                        newList.RemoveAt(rand1);
+                        newList.RemoveAt(rand2);
+                        break;
+
+                    case 2:
+                        history.Add(newList[rand1].ToString() + " * " + newList[rand2].ToString() + " = " + (newList[rand1] * newList[rand2]));
+                        newList.Add(newList[rand1] * newList[rand2]);
+                        newList.RemoveAt(rand1);
+                        newList.RemoveAt(rand2);
+                        break;
+
+                    case 3:
+                        if (newList[rand1] > newList[rand2])
+                        {
+                            history.Add(newList[rand1] + " - " + newList[rand2] + " = " + (newList[rand1] - newList[rand2]));
+                            newList.Add(newList[rand1] - newList[rand2]);
+                            newList.RemoveAt(rand1);
+                            newList.RemoveAt(rand2);
+                            break;
+                        }
+                        else
+                        {
+                            history.Add(newList[rand2].ToString() + " - " + newList[rand1].ToString() + " = " + (newList[rand2] - newList[rand1]));
+                            newList.Add(newList[rand2] - newList[rand1]);
+                            newList.RemoveAt(rand1);
+                            newList.RemoveAt(rand2);
+                            break;
+                        }
+
+                    case 4:
+                        if (newList[rand1] > newList[rand2])
+                        {
+                            if ((newList[rand1] / newList[rand2] % 1) == 0)
+                            {
+                                history.Add(newList[rand1].ToString() + " / " + newList[rand2].ToString() + " = " + (newList[rand1] / newList[rand2]));
+                                newList.Add(newList[rand1] / newList[rand2]);
+                                newList.RemoveAt(rand1);
+                                newList.RemoveAt(rand2);
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            if ((newList[rand1] / newList[rand2] % 1) == 0)
+                            {
+                                history.Add(newList[rand2].ToString() + " / " + newList[rand1].ToString() + " = " + (newList[rand2] / newList[rand1]));
+                                newList.Add(newList[rand2] / newList[rand1]);
+                                newList.RemoveAt(rand1);
+                                newList.RemoveAt(rand2);
+                            }
+                            break;
+                        }
+                }
+            }
+            return newList[newList.Count - 1];
+        }
+    }
+}
